3399bdc3ae19ea3356f4bbeb41712a86e89059f1
瑩 佳 <a11910691110@gmail.com>
2021-03-30 01:45:20 +0800

    質數判斷

---
+++ 1HW/Prime.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1HW
+{
+    public partial class Prime : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int num = 2;
+            int cout = 100;
+            Response.Write("判段是否為質數:" + num + "</br>");
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    cout++;
+                }
+            }
+            if(cout == 2)
+            {
+                Response.Write("Y");
+            }
+            else
+            {
+                Response.Write("N");
+            }
+        }
+    }
+}
